312cd002e03a083d4f947423446feeb2c6c2bf2f
joaovalente2004 <84444718+joaovalente2004@users.noreply.github.com>
2022-12-06 11:48:05 +0000

    Delete htdocs directory

--- htdocs/olho.js
+++
@@ -1,9 +0,0 @@
-let btn = document.querySelector(".olho");
-btn.addEventListener("click", function () {
-  let input = document.querySelector("#senha");
-  if (input.getAttribute("type") == "password") {
-    input.setAttribute("type", "text");
-  } else {
-    input.setAttribute("type", "password");
-  }
-});
