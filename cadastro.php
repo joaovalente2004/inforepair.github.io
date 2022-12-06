cbae982db5c192c2006eba14446b438d6258cbb0
joaovalente2004 <84444718+joaovalente2004@users.noreply.github.com>
2022-12-06 11:48:52 +0000

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,36 @@
+<?php
+
+include("conectar.php");
+
+$email=$_POST['email'];
+$senha=$_POST['senha'];
+$telemovel=$_POST['telemovel'];
+$nome=$_POST['nome'];
+$sobrenome=$_POST['sobrenome'];
+
+$query1 = $conexao-> prepare("SELECT email, telemovel FROM cliente WHERE email = ? OR telemovel = ?");
+$query1-> bind_param("ss", $email, $telemovel);
+$query1-> execute();
+$query1-> store_result();
+
+if($query1-> num_rows <= 0)
+{
+    $query2 = $conexao-> prepare("INSERT INTO cliente (senha, telemovel, email, nome, sobrenome) VALUES (?, ?, ?, ?, ?)");
+    $query2-> bind_param("sssss", $senha, $telemovel, $email, $nome, $sobrenome);
+    $query2-> execute();
+    $query2-> store_result();
+    header("Location: http://localhost/index.php");
+}
+else
+{
+    
+    echo 'Já existe uma conta com este nome';
+    header("Location: http://localhost/registar_cliente.html");
+}
+
+?>
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+
+
+session_start();
+
+
+?>
+
+
+<!DOCTYPE html>
+
+<html lang="pt">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./css/edicao.css">
+
+  <title>INFO REPAIR</title>
+  <link rel="icon" type="imagem/png" href="./img/logo.png" />
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
+
+
+  <link href="bootstrap.min.css" rel="stylesheet">
+
+  <link href="navbar-top.css" rel="stylesheet">
+</head>
+
+<body class="cor">
+
+
+
+
+
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+      <img src="./img/Digital Marketing Logo.png" class="rounded mx-auto d-block" alt="...">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0 fs-4">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="conta_registador.php">Registador</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="registar_backoffice.html">BACK OFFICE</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="registar_cliente.html">Cliente</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="ola.php">ola</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+
+
+
+
+  <div class="position-relative">
+
+    <div class="conectore"><img src="./img/conectores.png"> </div>
+
+    <div class="reparar"><img src="./img/ol.jpg"> </div>
+
+  </div>
+
+
+
+
+
+
+
+  <div class="tres">
+    <img src="./img/Digital Marketing Logo.png" class="rounded float-start pt-4" alt="">
+  </div>
+
+
+
+
+
+
+
+
+
+
+  <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="main.js"></script>
+
+</body>
+
+
+</html>
